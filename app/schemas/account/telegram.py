6d1b9d53677b7b6959da792b15f10237415cd72d
Jay Hsia <jayhsia1997@gmail.com>
2024-02-01 16:50:55 +0800

    feat: Remove unnecessary fields

--- app/schemas/account/telegram.py
+++ app/schemas/account/telegram.py
@@ -1,9 +1,7 @@
 """
 Models for Telegram account
 """
-from datetime import datetime
 from typing import Optional
-from uuid import UUID
 
 from pydantic import BaseModel, Field, field_serializer
 
@@ -22,15 +20,6 @@ class TelegramAccount(BaseModel):
     is_bot: bool = Field(default=False, description="Is Bot")
     is_premium: bool = False
     link: Optional[str] = Field(default=None, description="Link")
-    created_at: Optional[datetime] = Field(default=None, description="Created At")
-    created_by: Optional[str] = Field(default=None, description="Created By")
-    created_by_id: Optional[UUID] = Field(default=None, description="Created By ID")
-    updated_at: Optional[datetime] = Field(default=datetime.utcnow(), description="Updated At")
-    updated_by: Optional[str] = Field(default=None, description="Updated By")
-    updated_by_id: Optional[UUID] = Field(default=None, description="Updated By ID")
-    delete_reason: Optional[str] = Field(default=None, description="Delete Reason")
-    is_deleted: bool = Field(default=False, description="Is Deleted")
-    description: Optional[str] = Field(default=None, description="Description")
 
 
 class TelegramChatGroup(BaseModel):
@@ -41,15 +30,6 @@ class TelegramChatGroup(BaseModel):
     type: str
     in_group: bool
     bot_type: BotType
-    created_at: Optional[datetime] = Field(default=None, description="Created At")
-    created_by: Optional[str] = Field(default=None, description="Created By")
-    created_by_id: Optional[UUID] = Field(default=None, description="Created By ID")
-    updated_at: Optional[datetime] = Field(default=datetime.utcnow(), description="Updated At")
-    updated_by: Optional[str] = Field(default=None, description="Updated By")
-    updated_by_id: Optional[UUID] = Field(default=None, description="Updated By ID")
-    delete_reason: Optional[str] = Field(default=None, description="Delete Reason")
-    is_deleted: bool = Field(default=False, description="Is Deleted")
-    description: Optional[str] = Field(default=None, description="Description")
 
     @field_serializer("bot_type")
     def serialize_enum(self, value: BotType, _info):
